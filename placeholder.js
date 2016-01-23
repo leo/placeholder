79ddf69431c44cbb069320cc7decca5a8ca9a9bc
Leonard Lamprecht <mindrun@icloud.com>
2016-01-23 12:15:23 +0100

    Use spaces for indention

--- placeholder.js
+++ placeholder.js
@@ -2,94 +2,94 @@
 
 window.onload = function() {
 
-	if( window.navigator.userAgent.indexOf('MSIE') > 0 ) {
+  if( window.navigator.userAgent.indexOf('MSIE') > 0 ) {
 
-		var mainSlug = 'placeholder', dataSlug = 'data-' + mainSlug;
-		var items = document.querySelectorAll( '[' + mainSlug + ']' );
+    var mainSlug = 'placeholder', dataSlug = 'data-' + mainSlug;
+    var items = document.querySelectorAll( '[' + mainSlug + ']' );
 
-		for ( var i = 0; i < items.length; i++ ) {
+    for ( var i = 0; i < items.length; i++ ) {
 
-			var item = items[i];
+      var item = items[i];
 
-			function addClass(what) {
+      function addClass(what) {
 
-				if ( what.className == '' ) {
-					what.className = mainSlug + '-on';
-				} else {
-					what.className = what.className + ' ' + mainSlug + '-on';
-				}
+        if ( what.className == '' ) {
+          what.className = mainSlug + '-on';
+        } else {
+          what.className = what.className + ' ' + mainSlug + '-on';
+        }
 
-			}
+      }
 
-			function form() {
+      function form() {
 
-				while ( item.parentNode ) {
+        while ( item.parentNode ) {
 
-					par = item.parentNode;
+          par = item.parentNode;
 
-					if ( par.tagName === 'FORM' ) {
-						return par;
-					}
+          if ( par.tagName === 'FORM' ) {
+            return par;
+          }
 
-					return null;
+          return null;
 
-				}
+        }
 
-			}
+      }
 
-			item.value = item.getAttribute( mainSlug );
-			item.removeAttribute( mainSlug );
-			item.setAttribute( dataSlug, item.value );
+      item.value = item.getAttribute( mainSlug );
+      item.removeAttribute( mainSlug );
+      item.setAttribute( dataSlug, item.value );
 
-			addClass( item );
+      addClass( item );
 
-			item.onfocus = function() {
+      item.onfocus = function() {
 
-				var ph = this.getAttribute( dataSlug ), cl = this.className;
+        var ph = this.getAttribute( dataSlug ), cl = this.className;
 
-				if ( cl.indexOf( ' ' ) > -1 ) {
-					var cl = cl.replace( mainSlug + '-on', '' );
-				} else {
-					this.removeAttribute( 'class' );
-				}
+        if ( cl.indexOf( ' ' ) > -1 ) {
+          var cl = cl.replace( mainSlug + '-on', '' );
+        } else {
+          this.removeAttribute( 'class' );
+        }
 
-				if( this.value == ph || this.getAttribute( 'value' ) == ph ) {
-					this.value = '';
-				}
+        if( this.value == ph || this.getAttribute( 'value' ) == ph ) {
+          this.value = '';
+        }
 
-				this.onblur = function() {
+        this.onblur = function() {
 
-					if( this.value.length == 0 ) {
-						addClass( this );
-						this.value = this.getAttribute( dataSlug );
-					}
+          if( this.value.length == 0 ) {
+            addClass( this );
+            this.value = this.getAttribute( dataSlug );
+          }
 
-				};
+        };
 
-			};
+      };
 
-			if( form() ) {
+      if( form() ) {
 
-				form().onsubmit = function() {
+        form().onsubmit = function() {
 
-					var inputs = this.querySelectorAll( '[' + dataSlug + ']' );
+          var inputs = this.querySelectorAll( '[' + dataSlug + ']' );
 
-					for( var i = 0; i < inputs.length; i++ ) {
+          for( var i = 0; i < inputs.length; i++ ) {
 
-						if( inputs[i].value == inputs[i].getAttribute( dataSlug ) ) {
-							inputs[i].value = '';
-						}
+            if( inputs[i].value == inputs[i].getAttribute( dataSlug ) ) {
+              inputs[i].value = '';
+            }
 
-					}
+          }
 
-					return true;
+          return true;
 
-				};
+        };
 
-			}
+      }
 
-		}
+    }
 
-	}
+  }
 
 }
